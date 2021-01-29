1f8498731a6eb773d16bc24fbc584aa04e2dd2c3
sankalp98 <sankalp.kasle@gmail.com>
2021-01-29 13:38:34 +0530

    nc

--- core/asgi.py
+++ core/asgi.py
@@ -6,7 +6,7 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
-
+"""
 import os
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
@@ -29,3 +29,11 @@ application = ProtocolTypeRouter({
         )
     ),
 })
+"""
+import os
+import django
+from channels.routing import get_default_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
+application = get_default_application()
